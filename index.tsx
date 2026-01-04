c9bc47c97afc691a9c9f1add201f93d0fde04a2a
kleenrr-app <kleenrr.app@gmail.com>
2026-01-04 11:30:33 -0500

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,11 @@
+
+import ServiceSelector from '../components/ServiceSelector';
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">kleenrr</h1>
+      <ServiceSelector />
+    </main>
+  );
+}
