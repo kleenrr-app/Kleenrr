242deceb7ebc634b3b4babaf090e78def665c663
kleenrr-app <kleenrr.app@gmail.com>
2026-01-04 11:33:43 -0500

    Add files via upload

---
+++ ServiceSelector.tsx
@@ -0,0 +1,10 @@
+
+export default function ServiceSelector() {
+  return (
+    <div className="grid grid-cols-3 gap-4 mt-6">
+      <button>❄ Snow</button>
+      <button>🌱 Lawn</button>
+      <button>🚗 Vehicle</button>
+    </div>
+  );
+}
